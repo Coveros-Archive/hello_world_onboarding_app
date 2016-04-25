87b89085e519d495cf8174900f9544cbe59cef01
hammid <hammid.funsho@coveros.com>
2016-04-25 11:14:57 -0400

    Adding required files

---
+++ EvenNumberGeneratorTest.java
@@ -0,0 +1,18 @@
+//testing the Even Number Generator application
+package com.coveros.hello_world_tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.coveros.hello_world.EvenNumberGenerator;
+
+
+public class EvenNumberGeneratorTest {
+
+	@Test
+   public void testRandomEvenNumberGenerator() {
+	   
+      int randomResult = EvenNumberGenerator.generateRandomEven();
+      
+      assertEquals(0,randomResult%2);
+   }
+}
