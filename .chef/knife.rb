2629131863ec16b3ba0e31a2873b440da24df82c
hammid <hammid.funsho@coveros.com>
2016-05-02 14:04:23 -0400

    more updates to chef config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,10 @@
 # See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
-#current_dir = File.dirname(__FILE__)
-#log_level                :info
-#log_location             STDOUT
-#node_name                "jsmith"
-#client_key               "#{current_dir}/jsmith.pem"
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                 "chef_node"
+client_key               "home/centos/HF_Devops_BootcampII.pem"
 #validation_client_name   "4thcoffee-validator"
 #validation_key           "#{current_dir}/4thcoffee-validator.pem"
 #chef_server_url          "https://ec2-54-210-232-98.compute-1.amazonaws.com/organizations/4thcoffee"
